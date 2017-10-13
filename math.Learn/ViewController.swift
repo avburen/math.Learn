e3a223179c9c71608f6e1c927c8593c2543104b9
kevin578 <kbriggs@aapl000894.local>
2017-10-13 07:50:03 -0400

    3 todos added

--- math.Learn/ViewController.swift
+++ math.Learn/ViewController.swift
@@ -10,27 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var equation: UILabel!
+    @IBOutlet weak var checkAnswer: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textInput: UITextField!
+    
+    //declaring variables for the randomEquation function
+    var number1 = 0
+    var number2 = 0
+    var correctAnswer = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        equation.isHidden = false
+        textInput.isHidden = false
+        checkAnswer.isHidden = false
+        nextButton.isHidden = true
         label.text = ""
-        
            }
-    @IBOutlet weak var label: UILabel!
 
 
-
-
-    @IBAction func label2(_ sender: Any) {
-        label.text = "Incorrect"
+    @IBAction func checkAnswer(_ sender: Any) {
+        //TODO #2 - Use an if/then/else statement to check if the answer is correct. Get the value of the text input with textInput.text. Then show and hide the correct buttons.
+        
     }
-
-    @IBAction func label3(_ sender: Any) {
-        label.text = "Correct!"
+    @IBAction func nextButton(_ sender: Any) {
+        //TODO #3 - Show and hide the correct buttons when the next button is pressed.
     }
-
-    @IBOutlet weak var buttoncheck: UILabel!
     
-    @IBOutlet weak var buttonnext: UIButton!
+
+
+    
+    
+    func randomEquation(){
+        
+        //TODO #1 - fill in this function with the random number generator from the playground
+
+    }
 }
+
+
 
 
