2a505763b25a13815008092c43afb02d4f347c37
Nolan Byron <nbyron@aapl000610.local>
2017-10-17 10:48:26 -0400

    today we began to design our app cover and worked on more coding.

--- math.Learn/MyPlayground_math.learn.playground/Contents.swift
+++ math.Learn/MyPlayground_math.learn.playground/Contents.swift
@@ -1,23 +1,45 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
 var str = "Hello, playground"
 
 
 var number1 = Int(arc4random_uniform(UInt32(100)))
-print (number1)
+
 
 var number2 = Int(arc4random_uniform(UInt32(100)))
-print(number2)
 
-var op = Int(arc4random_uniform(UInt32(2)))
-print(op)
+
+var op = Int(arc4random_uniform(UInt32(3)))
+
 
 var correctAnswer = 0
 
-if (op == 1) {
+if (op == 0) {
     correctAnswer = number1 + number2
-    print("\(number1) * \(number2)")
+    print("\(number1) + \(number2)")
 }
 
+
+
+
+
+
+
+else if (op == 1) {
+    correctAnswer = number1 - number2
+    print("\(number1) - \(number2)")
+    
+}
+
+else if (op == 2) {
+    correctAnswer = number1 - number1 - number2
+    print("\number1)*\(number2)")
+}
+
+else if (op == 2) {
+    correctAnswer = number1 * number2
+    print("\(number1) * \(number2)")
+    
+}
+
+print(correctAnswer)
--- math.Learn/ViewController.swift
+++ math.Learn/ViewController.swift
@@ -29,16 +29,30 @@ class ViewController: UIViewController {
         textInput.isHidden = false
         checkAnswer.isHidden = false
         nextButton.isHidden = true
-        label.text = ""
-           }
+        label.isHidden = true
+        //randomEquation()
+    }
 
 
     @IBAction func checkAnswer(_ sender: Any) {
         //TODO #2 - Use an if/then/else statement to check if the answer is correct. Get the value of the text input with textInput.text. Then show and hide the correct buttons.
         
+        if Int(textInput.text!) == correctAnswer {
+        print ("Correct!")
+        }
+        label.isHidden = false
+        equation.isHidden = true
+        checkAnswer.isHidden = true
+        nextButton.isHidden = false
+        
     }
     @IBAction func nextButton(_ sender: Any) {
         //TODO #3 - Show and hide the correct buttons when the next button is pressed.
+        label.isHidden = true
+        equation.isHidden = false
+        checkAnswer.isHidden = false
+        nextButton.isHidden = true 
+        
     }
     
 
@@ -46,8 +60,48 @@ class ViewController: UIViewController {
     
     
     func randomEquation(){
+         number1 = Int(arc4random_uniform(UInt32(100)))
         
+        
+        number2 = Int(arc4random_uniform(UInt32(100)))
+
+        
+        
+        let op = Int(arc4random_uniform(UInt32(3)))
+        
+        
+         correctAnswer = 0
+        if (op == 0) {
+            correctAnswer = number1 + number2
+            equation.text = "\(number1) + \(number2)"
+        }
+            
+            
+            
+            
+            
+            
+            
+        else if (op == 1) {
+            correctAnswer = number1 - number2
+            equation.text = ("\(number1) - \(number2)")
+            
+        }
+            
+        else if (op == 2) {
+            correctAnswer = number1 * number2
+            equation.text = ("\(number1)*\(number2)")
+        }
+            
+        else if (op == 2) {
+            correctAnswer = number1 * number2
+            equation.text = ("\(number1) * \(number2)")
+            
+        }
+        
+        print(correctAnswer)
         //TODO #1 - fill in this function with the random number generator from the playground
+        
 
     }
 }
